6edb9898c6d242121a87c9b4972e6639342f571e
hs-himzheuski <vitali.himzheuski@headspace.com>
2023-12-31 17:24:30 +0100

    update path

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -3,7 +3,9 @@ const API_PATHS = {
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   import: "https://j1hl0ceqk0.execute-api.eu-west-1.amazonaws.com/prod",
   bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  // cart: "http://localhost:4000/api",
+  // cart: "http://jimmba-cart-api-development.eu-west-1.elasticbeanstalk.com/api",
+  cart: "https://q2pdox4j25.execute-api.eu-west-1.amazonaws.com/api",
 };
 
 export default API_PATHS;
